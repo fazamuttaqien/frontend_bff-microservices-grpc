874ad0146d0f2fe1eae8d1df82788bc843a57d23
Faza Muttaqien <fazamttqn@gmail.com>
2026-08-30 20:16:59 +0700

    feat(frontend): improve UX and error handling

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import { App } from './app/App'
 import { store } from './app/store'
 import './index.css'
-import './features/products/products.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
