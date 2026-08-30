ed39fd2a4e051123d4449391553d46f6cab2484f
Faza Muttaqien <fazamttqn@gmail.com>
2026-08-30 13:51:48 +0700

    feat: add public layout

---
+++ src/app/layouts/PublicLayout.tsx
@@ -0,0 +1,17 @@
+import { NavLink, Outlet } from 'react-router-dom'
+
+export function PublicLayout() {
+  return (
+    <div>
+      <header>
+        <nav aria-label="Public navigation">
+          <NavLink to="/">Home</NavLink> | <NavLink to="/login">Login</NavLink> |{' '}
+          <NavLink to="/register">Register</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
