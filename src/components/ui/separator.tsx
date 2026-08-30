2b09a5a0137d23a69fd57c5ad6d901f375050a03
Faza Muttaqien <fazamttqn@gmail.com>
2026-08-30 17:06:20 +0700

    fix(ui): keep separator dependency free

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
-import * as SeparatorPrimitive from '@radix-ui/react-separator'
 import { cn } from '@/lib/utils'
 
-function Separator({ className, orientation = 'horizontal', decorative = true, ...props }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
-  return <SeparatorPrimitive.Root data-slot="separator" decorative={decorative} orientation={orientation} className={cn('bg-border shrink-0', orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px', className)} {...props} />
+function Separator({ className, orientation = 'horizontal', ...props }: React.HTMLAttributes<HTMLDivElement> & { orientation?: 'horizontal' | 'vertical' }) {
+  return <div role="separator" aria-orientation={orientation} data-slot="separator" className={cn('bg-border shrink-0', orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px', className)} {...props} />
 }
 
 export { Separator }
