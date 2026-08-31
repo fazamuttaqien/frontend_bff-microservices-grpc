dc4c4bf9848739c8941351f0ce09ac46b192e3a8
Faza Muttaqien <fazamttqn@gmail.com>
2026-08-31 11:17:21 +0700

    feat(observability): initialize Faro before app startup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { App } from './app/App'
 import { store } from './app/store'
+import './observability/faro'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
